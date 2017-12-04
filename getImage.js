31bfc0ea4c53c28e104b9f537711c046eafa9b24
Mehmet Mücahit ÇALI <mucahit8cali@gmail.com>
2017-12-04 03:25:05 +0300

    Add files via upload

---
+++ getImage.js
@@ -0,0 +1,11 @@
+$(function(){
+	
+chrome.tabs.query({
+    active: true,
+    currentWindow: true
+}, function(tabs) {
+    var tabURL = tabs[0].url;
+	var urlParse=tabURL.substring(32,43);
+    chrome.tabs.create({'url':'https://img.youtube.com/vi/'+urlParse+'/maxresdefault.jpg'});
+});
+});
